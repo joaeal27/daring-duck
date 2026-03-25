84e589e7285352938ccf05fe143b03df58ea5bbd
joaeal27 <joaeal27@bergen.org>
2026-03-24 22:41:53 -0400

    starting to make methods

--- Tape.java
+++ Tape.java
@@ -1,6 +1,11 @@
 public class Tape {
     private String startingInput;
+
     public Tape(String startingInput){
+        this.startingInput = startingInput;
+    }
+
+    public void left(){
         
     }
 }
