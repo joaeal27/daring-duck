0ebaebfdf7f09eafd3de0912625e187fa70ad153
1nayrryan <155669762+1nayrryan@users.noreply.github.com>
2026-03-25 13:38:39 -0400

    added descriptions

--- Tape.java
+++ Tape.java
@@ -1,6 +1,9 @@
 public class Tape {
     private String startingInput;
-
+    //receives a starting input in its constructor, and can go left, right, read, and write. 
+    //The Tape class should automatically resize when it discovers that more tape is needed. 
+    // It should also have some sort of toString() or oneCount() method so that we can all see the output when the machine is done running!
+    
     public Tape(String startingInput){
         this.startingInput = startingInput;
     }
