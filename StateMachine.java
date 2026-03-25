74eba5ff670bac9d51abe84fb8e0a2f4f9acf173
1nayrryan <155669762+1nayrryan@users.noreply.github.com>
2026-03-25 13:41:49 -0400

    directions to files

--- StateMachine.java
+++ StateMachine.java
@@ -1,3 +1,4 @@
 public class StateMachine {
-    
+    //A StateMachine class that holds all of the states, keeps track of what the current state is, 
+    // and given an input, moves to the next state and tells the tape to move left and right.
 }
--- Transition.java
+++ Transition.java
@@ -1,3 +1,4 @@
 public class Transition {
-    
+    //A Transition class that keeps track of a tape symbol as input and the results: 
+    // what state to move to next, what to write, and whether to move the tape left or right.
 }
