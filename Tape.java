fb9825ff37cc6d9d8ec9ee6674703625a4759509
1nayrryan <155669762+1nayrryan@users.noreply.github.com>
2026-03-25 13:47:46 -0400

    push

--- Tape.java
+++ Tape.java
@@ -4,9 +4,14 @@ public class Tape {
     //The Tape class should automatically resize when it discovers that more tape is needed. 
     // It should also have some sort of toString() or oneCount() method so that we can all see the output when the machine is done running!
     
+<<<<<<< Updated upstream
     // 15/ (s + a +)
     //reccomend: 0, 1, 2 alphabet
     //see a 2, replace a 1 then write 2 1's at the end
+=======
+
+    //tape will be an array list, allows you to read, write edit
+>>>>>>> Stashed changes
     public Tape(String startingInput){
         this.startingInput = startingInput;
     }
--- TuringMachineRunner.java
+++ TuringMachineRunner.java
@@ -1,3 +1,4 @@
 public class TuringMachineRunner {
     //A TuringMachineRunner class that has main, manually assembles your chosen machine, runs the Turing Machine, checks whether each new state is a final state, and does something reasonable with the output when the final state is reached.
+    
 }
